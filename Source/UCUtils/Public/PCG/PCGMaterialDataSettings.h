34cfb9bc72a6cd3630c8746552f2da78f4d69327
JohnJFarrow <105021048+JohnJFarrow@users.noreply.github.com>
2026-03-09 12:13:54 +1300

    started GetMaterialDataSettings

---
+++ Source/UCUtils/Public/PCG/PCGMaterialDataSettings.h
@@ -0,0 +1,41 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Elements/PCGDataFromActor.h"
+
+#include "PCGMaterialDataSettings.generated.h"
+
+struct FPCGGetDataFunctionRegistryOutput;
+struct FPCGGetDataFunctionRegistryParams;
+
+UCLASS(MinimalAPI, BlueprintType, ClassGroup = (Procedural))
+class UPCGGetMaterialDataSettings : public UPCGDataFromActorSettings
+{
+	GENERATED_BODY()
+
+public:
+
+	UPCGGetMaterialDataSettings();
+
+	//~Begin UPCGSettings interface
+	#if WITH_EDITOR
+	virtual FName GetDefaultNodeName() const override;
+	virtual FText GetDefaultNodeTitle() const override;
+	#endif
+
+protected:
+	virtual TArray<FPCGPinProperties> OutputPinProperties() const override;
+	//~End UPCGSettings interface
+
+	//~Begin UPCGDataFromActorSettings interface
+public:
+	virtual EPCGDataType GetDataFilter() const override
+	{
+		return EPCGDataType::Other;
+	}
+
+protected:
+	//~End UPCGDataFromActorSettings
+};
+
