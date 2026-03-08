f4e40aaf763abd868ff228523d3491e1b146db19
JohnJFarrow <105021048+JohnJFarrow@users.noreply.github.com>
2026-03-08 14:10:11 +1300

    init

---
+++ Source/UCUtils/Private/UCUtils.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UCUtils.h"
+
+#define LOCTEXT_NAMESPACE "FUCUtilsModule"
+
+void FUCUtilsModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FUCUtilsModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUCUtilsModule, UCUtils)
